a210bd3f2d90c139aac86802551171d71c0abc52
barteek-poz <szowior4@wp.pl>
2024-02-06 21:12:04 +0100

    finished responsive part of project

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-const burger = document.querySelector('.burger');
+const burger = document.querySelector(".burger");
+const mobileMenu = document.querySelector(".nav-menu-mobile");
+const navLinks = document.querySelectorAll(".nav-menu-link");
 
-burger.addEventListener('click', function() {
-    burger.classList.toggle('burger-isActive')
-})
+burger.addEventListener("click", function () {
+  burger.classList.toggle("burger-isActive");
+  mobileMenu.classList.toggle("nav-menu-mobile-active");
+});
+navLinks.forEach(function (navLink) {
+  navLink.addEventListener("click", function () {
+    burger.classList.remove("burger-isActive");
+    mobileMenu.classList.remove("nav-menu-mobile-active");
+  });
+});
+
+
