8eb3c9b836cdd21648b72915a37d76b8d6f5a94c
barteek-poz <szowior4@wp.pl>
2024-03-03 21:57:20 +0100

    fixed mobile nav

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
+const menu = document.querySelector("#menu");
 const burger = document.querySelector(".burger");
 const mobileMenu = document.querySelector(".nav-menu-mobile");
 const navLinks = document.querySelectorAll(".nav-menu-link");
 
 burger.addEventListener("click", function () {
   burger.classList.toggle("burger-isActive");
-  mobileMenu.classList.toggle("nav-menu-mobile-active");
+  menu.classList.toggle("nav-menu");
+  menu.classList.toggle("nav-menu-mobile");
 });
+
 navLinks.forEach(function (navLink) {
   navLink.addEventListener("click", function () {
     burger.classList.remove("burger-isActive");
-    mobileMenu.classList.remove("nav-menu-mobile-active");
   });
 });
-
-
